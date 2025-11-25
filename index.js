9acd4d3be5a95b99242e6ac2d82808c4536d67f4
Yasir45455 <149455805+Yasir45455@users.noreply.github.com>
2025-11-25 12:24:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,23 +17,7 @@ const promoImagesRoute = require('./routes/promoImages');
 
 const app = express();
 
-// CORS setup for multiple origins
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://wasif-ali47.github.io/foodiefly" // add more origins if needed
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps, curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 const port = process.env.PORT || 3001;
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
